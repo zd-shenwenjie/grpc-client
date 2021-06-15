b71eaf1ec09f114eb108aece8bf59a724c7cd1d4
shenwenjie <wenjie.shen@zd-automotive.de>
2021-06-15 11:35:23 +0200

    modify compile-proto.sh

--- node/scripts/compile-proto.sh
+++ node/scripts/compile-proto.sh
@@ -1,13 +1,14 @@
 #!/usr/bin/env bash
-
-OUT_DIR="./src/lib"
+OUT_DIR="./src/lib/proto"
 IN_DIR="./proto"
 PROTOC="$(npm bin)/grpc_tools_node_protoc"
 PROTOC_GEN_TS_PATH="$(npm bin)/protoc-gen-ts"
 PROTOC_GEN_GRPC_PATH="$(npm bin)/grpc_tools_node_protoc_plugin"
 
+mkdir -p $OUT_DIR
+
 $PROTOC \
-    -I="./" \
+    -I="./proto/" \
     --plugin=protoc-gen-ts=$PROTOC_GEN_TS_PATH \
     --plugin=protoc-gen-grpc=$PROTOC_GEN_GRPC_PATH \
     --js_out=import_style=commonjs:$OUT_DIR \
